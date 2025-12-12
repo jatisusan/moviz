379a855604387e304271cfb71f90ff95b2f3cf9e
jatisusan <susurrus112@gmail.com>
2025-12-12 18:04:33 +0545

    Implement login ui

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,12 +1,79 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 
-const login = () => {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = () => {
+    // Handle login logic here
+  };
   return (
-    <View>
-      <Text>login</Text>
-    </View>
-  )
-}
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0"
+        resizeMode="cover"
+      />
+      <View className="flex-1 px-6">
+        <Image source={icons.logo} className="mx-auto w-12 h-10 mb-5 mt-20" />
+        <View className="mt-20">
+          <Text className="mb-10 text-center text-xl font-bold tracking-tight text-white">
+            Log in to your account
+          </Text>
 
-export default login
+          <TextInput
+            value={email}
+            onChangeText={setEmail}
+            placeholder="Email"
+            placeholderTextColor="#a8b5db"
+            className="bg-dark-200 px-6 py-5 rounded-lg text-white mb-6 "
+          />
+          <TextInput
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            placeholder="Password"
+            placeholderTextColor="#a8b5db"
+            className="bg-dark-200 px-5 py-5 rounded-lg text-white mb-6"
+          />
+
+          <TouchableOpacity
+            onPress={handleSubmit}
+            className=" bg-accent rounded-lg py-3.5 flex flex-row items-center justify-center mt-4"
+          >
+            <Text className="text-white text-base font-semibold">Login</Text>
+          </TouchableOpacity>
+
+          <View className="flex-row items-center justify-center mt-8">
+            <Text className=" text-light-200 font-semibold">
+              Don&apos;t have an account?
+            </Text>
+            <TouchableOpacity onPress={() => router.push("/(auth)/signup")}>
+              <Text className=" text-accent font-semibold ml-2">Sign Up</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+
+      <TouchableOpacity
+        onPress={() => router.push("/")}
+        className="absolute bottom-10 right-0 mx-5 bg-dark-200 rounded-lg py-3.5 px-5 flex flex-row items-center justify-center z-50"
+      >
+        <Image
+          source={icons.arrow}
+          className="size-5 mr-1 mt-0.5 rotate-180"
+          tintColor="#a8b5db"
+        />
+        <Text className="text-[#a8b5db] text-base font-semibold">Home</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Login;
